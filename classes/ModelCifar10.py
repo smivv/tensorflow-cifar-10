4576e735b9d728c6899fac28bb04db7a18864fd8
Vladimir <jeusveus@gmail.com>
2017-11-17 11:36:46 +0300

    Hyperparameters added.

--- classes/ModelCifar10.py
+++ classes/ModelCifar10.py
@@ -1,23 +1,76 @@
 import tensorflow as tf
 import numpy as np
+import logging
 
 
 class ModelCifar10:
+    """
+    A two-layer fully-connected neural network. The net has an input dimension of
+    N, a hidden layer dimension of H, and performs classification over C classes.
+    We train the network with a softmax loss function and L2 regularization on the
+    weight matrices. The network uses a ReLU nonlinearity after the first fully
+    connected layer.
+
+    In other words, the network has the following architecture:
+
+    input - fully connected layer - ReLU - fully connected layer - softmax
+
+    The outputs of the second fully-connected layer are the scores for each class.
+    """
 
     """--------------------------------------------------------------------------------------------------------------"""
 
     def __init__(self, input_size, hidden_size, output_size, std=1e-4):
+        """
+        Initialize the model. Weights are initialized to small random values and
+        biases are initialized to zero. Weights and biases are stored in the
+        variable self.params, which is a dictionary with the following keys:
+
+        W1: First layer weights; has shape (D, H)
+        b1: First layer biases; has shape (H,)
+        W2: Second layer weights; has shape (H, C)
+        b2: Second layer biases; has shape (C,)
+
+        Inputs:
+        - input_size: The dimension D of the input data.
+        - hidden_size: The number of neurons H in the hidden layer.
+        - output_size: The number of classes C.
+        """
 
         self._input_size = int(input_size)
         self._hidden_size = int(hidden_size)
         self._output_size = int(output_size)
 
-        # Hyperparameters
-        self.W1 = std * np.random.randn(self._input_size, self._hidden_size)
-        self.W2 = std * np.random.randn(self._hidden_size, self._output_size)
+        # Input
+        self.x = tf.placeholder(tf.float32, [None, self._input_size])
 
-        self.b1 = np.zeros(self._hidden_size)
-        self.b2 = np.zeros(self._output_size)
+        # Layer 1
+        with tf.variable_scope('layer1'):
+            # Hyperparameters
+            self.W1 = tf.get_variable('W1', [self._input_size, self._hidden_size], initializer=tf.random_normal_initializer())
+            self.b1 = tf.get_variable('b1', [self._hidden_size, ], initializer=tf.random_normal_initializer())
+            # Activation
+            self.y1 = tf.nn.relu(tf.matmul(self.x, self.W1) + self.b1)
+
+        # Layer 2
+        with tf.variable_scope('layer2'):
+            # Hyperparameters
+            self.W2 = tf.get_variable('W2', [self._hidden_size, self._output_size], initializer=tf.random_normal_initializer())
+            self.b2 = tf.get_variable('b2', [self._output_size, ], initializer=tf.random_normal_initializer())
+            # Activation
+            self.y2 = tf.nn.relu(tf.matmul(self.y1, self.W2) + self.b2)
+
+        # Add an op to initialize the variables.
+        self.init_op = tf.global_variables_initializer()
+
+        logging.info('Output layer size argument passed..')
+
+    """--------------------------------------------------------------------------------------------------------------"""
+
+    def launch(self):
+        with tf.Session() as sess:
+            # Run the init operation.
+            sess.run(self.init_op)
 
     """--------------------------------------------------------------------------------------------------------------"""
 
--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ from classes import ModelCifar10
 
 logging.basicConfig(level=logging.INFO)
 
+
 def serve(config):
     model = ModelCifar10.ModelCifar10(config.input_size, config.hidden_size, config.output_size)
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
