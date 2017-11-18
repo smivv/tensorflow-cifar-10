60f4b990bfec03e185b9fe0aa2c4803e6edc3244
Vladimir Smirnov <vovkasmirnov@yahoo.com>
2017-11-18 21:53:56 +0300

    Utils class created. Download dataset function realized.

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ virtualenv -p python3 .env
 source .env/bin/activate
 
 # installation dependencies
-pip install numpy
 pip install tensorflow
+pip install tqdm
 deactivate
 
-# get CIFAR10 dataset
+# get CIFAR10 dataset, now it's ambiguous
 wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xzvf cifasr-10-python.tar.gz
+tar -xzvf cifar-10-python.tar.gz
 rm cifar-10-python.tar.gz
